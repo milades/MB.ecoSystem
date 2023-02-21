e2338ee8b0ede5183716080847dd768173017b9e
Milad Esmaeili <miladesmaeili@hotmail.com>
2023-02-21 22:06:08 +0330

    ----

--- MB.WebApi/Program.cs
+++ MB.WebApi/Program.cs
@@ -1,12 +1,29 @@
+using MB.AuthDefinition.Entities.Identity;
+using MB.WebApi.Data.dbContext;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Identity.Web;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+
+builder.Services.AddDbContext<mb_db_context>(options =>
+    options.UseSqlServer(connectionString, b => b.MigrationsAssembly(typeof(mb_db_context).Assembly.FullName)));
+
+builder.Services.AddIdentity<Users, Roles>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<mb_db_context>().AddDefaultTokenProviders();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 
 // Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
